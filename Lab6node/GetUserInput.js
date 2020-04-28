191e0b2192408c3403ee8b8f33557d0eadf918d0
pwill1998 <35737694+pwill1998@users.noreply.github.com>
2020-04-28 12:06:58 -0400

    Add files via upload

---
+++ Lab6node/GetUserInput.js
@@ -0,0 +1,41 @@
+var http = require("http");
+var fs = require("fs");
+var os = require("os");
+var ip = require('ip');
+//var time = require("uptime");
+var server= http.createServer(function(req,res){
+    if(req.url==="/") {
+    fs.readFile("C:/Users/Administrator/Desktop/Lab6node/index.html","UTF-8",function(err,body){
+        res.writeHead(200,{"Content-Type":"Text/html"});
+        res.end(body);
+    });
+}
+else if (req.url.match("/sysinfo")){
+    myHostName=os.hostname();    
+html=`
+    <!DOCTYPE html>
+    <html>
+        <head>
+        <title>Node JS  Response </title>
+        </head>
+        <body>
+            <p>Hostname:${myHostName}</p>
+            <p>IP:${ip.address()}</p>
+            <p>Server Uptime:Days:${os.uptime()/86400},Hours:${os.uptime()/3600},Min:${os.uptime()/60},Sec:${os.uptime()}</p>
+            <p>Total Memory: ${os.totalmem()/8} MB </p>
+            <p>Free Memory: ${os.freemem()/8} MB</p>
+            <p>Number of CPUs:${os.cpus().length} </p>
+            
+            
+        </body>
+</html>`
+res.writeHead(200,{"Content-Type":"text/html"});
+res.end(html);
+}
+else{
+    res.writeHead(404,{"Content-Type":"text/plain"});
+    res.end(`404 File Not Found at ${req.url}`);
+}
+})
+server.listen(3000);
+console.log("Server listening on port 3000")
